7b4582ce80dcc73cd57943fa0971bad79b88b748
Sultan Naufal Abdillah <sultan.naufal@weekendinc.com>
2022-09-16 22:59:21 +0700

    add user.models.go

---
+++ day-4/task/models/user.models.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint           `json:"id" gorm:"primarykey"`
+	Name      string         `json:"name" form:"name"`
+	Email     string         `json:"email" form:"email" gorm:"index" validate:"required,email"`
+	Password  string         `json:"-" form:"password" validate:"required"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
